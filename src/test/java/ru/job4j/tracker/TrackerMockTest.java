f6204847c7e44cf2d83346f14e3fae5249641960
Veronika Lapenok <veronika.lapenok.yr@gmail.com>
2023-09-26 13:46:08 +0300

    1. What is a Mock object. [#504866]

---
+++ src/test/java/ru/job4j/tracker/TrackerMockTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.format.DateTimeFormatter;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
+
+public class TrackerMockTest {
+    private Output output = new StubOutput();
+    private MemTracker tracker = new MemTracker();
+    private Input input = mock(Input.class);
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+            "dd-MMMM-EEEE-yyyy HH:mm:ss");
+
+    @Test
+    public void whenDeleteItemMockTest() {
+        tracker.add(new Item("New item"));
+        DeleteAction deleteAction = new DeleteAction(output);
+
+        when(input.askInt(any(String.class))).thenReturn(1);
+
+        deleteAction.execute(input, tracker);
+
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo("=== Delete item ===" + ln
+                + "Заявка удалена успешно." + ln);
+    }
+
+    @Test
+    public void whenFindItemByIdMockTest() {
+        Item item = new Item("New item");
+        tracker.add(item);
+        var date = item.getCreated();
+        FindByIdAction findByIdAction = new FindByIdAction(output);
+
+        when(input.askInt(any(String.class))).thenReturn(1);
+
+        findByIdAction.execute(input, tracker);
+
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo("=== Find item by id ===" + ln
+                + "Item{id=1, name='New item', created=" + formatter.format(date) + "}" + ln);
+    }
+
+    @Test
+    public void whenFindItemByNameMockTest() {
+        Item item = new Item("New item");
+        tracker.add(item);
+        var date = item.getCreated();
+        FindByNameAction findByNameAction = new FindByNameAction(output);
+
+        when(input.askStr(any(String.class))).thenReturn("New item");
+
+        findByNameAction.execute(input, tracker);
+
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo("=== Find item by name ===" + ln
+                + "Item{id=1, name='New item', created=" + formatter.format(date) + "}" + ln);
+    }
+}
