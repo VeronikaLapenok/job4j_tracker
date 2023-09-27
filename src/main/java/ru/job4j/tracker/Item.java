7acf05971b8b88c864e75ee3afc5e3f9f0dda10d
Veronika Lapenok <veronika.lapenok.yr@gmail.com>
2023-09-27 14:25:35 +0300

    1. Hibernate configuration [#6872]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,20 @@
 package ru.job4j.tracker;
 
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Entity
+@Table(name = "items")
+@Data
 public class Item {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(
             "dd-MMMM-EEEE-yyyy HH:mm:ss");
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
@@ -25,30 +34,6 @@ public class Item {
     public Item(int id, String name, LocalDateTime created) {
         this.id = id;
         this.name = name;
-        this.created = created;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    public void setCreated(LocalDateTime created) {
         this.created = created;
     }
 
