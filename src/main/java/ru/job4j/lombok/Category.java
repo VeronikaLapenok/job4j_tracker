edcfd96827a67b97b4284a03ec55d352e025d983
Veronika Lapenok <veronika.lapenok.yr@gmail.com>
2023-09-27 12:25:52 +0300

    2. @Setter, @Getter and others [#504889]

---
+++ src/main/java/ru/job4j/lombok/Category.java
@@ -0,0 +1,14 @@
+package ru.job4j.lombok;
+
+import lombok.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Category {
+    @NonNull
+    @Getter
+    @EqualsAndHashCode.Include
+    private int id;
+    @Getter
+    @Setter
+    private String name;
+}
